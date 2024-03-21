78b8f5587045d7f621c2e7b1cb9905f44bb907b3
Okumura0903 <info@muworks.net>
2024-03-21 21:49:04 +0900

    api server complete

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 5000;
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express');
-});
+app.use(express.json());
+app.use('/api/v1', require('./src/v1/routes/auth'));
+
+//DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log('DBと接続中');
+} catch (error) {
+  console.log(error);
+}
 
 app.listen(PORT, () => {
   console.log('ローカルサーバー起動中');
